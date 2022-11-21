1d32add53d9400c64b58301bbf04aed440b9f6a6
Harish Kumar <727721euit049@skcet.ac.in>
2022-11-21 11:33:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import react1 from './react1'
+class App extends Component {
+  render() {
+    return (
+        <div>
+        <react1/>        
+        </div>
+    );
+  }
+}
+
+export default App
---
+++ src/react1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class react1 extends Component {
+  render() {
+    return (
+      <div>
+       <h1>"hjgdgfhfufygru"</h1>
+      </div>
+    )
+  }
+}
+
+export default react1
